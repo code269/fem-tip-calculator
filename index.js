8940358b8810b030940650f51af5743b92c30c03
Zilong Guan <cerealbox269@gmail.com>
2026-01-26 17:08:23 -0500

    feat: add form attributes to HTML

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('Hello world');
+
+const getTipAmount = (bill, tipPercent, people) => {
+  return (bill * (tipPercent / 100)) / people;
+};
+
+const getTotalPerPerson = (bill, tipAmount, people) => {
+  return bill / people + tipAmount;
+};
+
+// Expect strings / floats
+function updateDisplay(bill, tipPercent, people) {
+  const tipAmount = getTipAmount(bill, tipPercent, people);
+  const totalPerPerson = getTotalPerPerson(bill, tipAmount, people);
+
+  console.log(`
+        Tip Amount: ${tipAmount}
+        Total: ${totalPerPerson}
+    `);
+}
+
+// Have constant event listeners (?) to update display accordingly to changes in form
